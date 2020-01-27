4d0eb9eb87350efa61d5af355d276f7c414ed520
Madison Harris <madisonharrisvt@gmail.com>
2020-01-27 13:11:03 -0800

    Madison: Used Express to show a list of users in Kano-provided database.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
+/**
+ * A sample set of calls to provide usage examples of the included supporting classes.
+ */
+
+const Database = require("../lib/Database");
+
+// Instantiate a new database
+const db = new Database();
+
+// Get a reference to the user collection
+const user = db.collection('user');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fart' });
+  res.render('index', { title: "Gift Exchange", users: user.data });
 });
 
 module.exports = router;
